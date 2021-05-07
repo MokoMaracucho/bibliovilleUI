f8a5a4aa3043880dfff1a4f698e6939e98164855
MokoMaracucho <desmoulin.federico@gmail.com>
2021-05-07 09:30:47 +0200

    RestTemplate - application.properties

---
+++ src/test/java/com/oc/bibliovilleUI/BibliovilleUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oc.bibliovilleUI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliovilleUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
